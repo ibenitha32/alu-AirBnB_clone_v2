df44990b12d6e8c6dd65420de4924db871212080
Benitha Iradukunda <116553054+ibenitha32@users.noreply.github.com>
2023-10-12 10:50:53 +0200

    updated the servers

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -9,7 +9,7 @@ from datetime import datetime
 from fabric.api import env, local, put, run, runs_once
 
 
-env.hosts = ['34.138.32.248', '3.226.74.205']
+env.hosts = ['54.91.25.87', '50.19.154.3']
 
 
 def do_deploy(archive_path):
