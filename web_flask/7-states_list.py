f707bc4ded4ec51fa573f010c793f363a66bf703
Benitha Iradukunda <116553054+ibenitha32@users.noreply.github.com>
2023-10-16 21:37:33 +0200

    doc

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
-"""Starts a flask app
-    listens to 0.0.0.0:5000
-    
+"""Flask Application for Displaying a List of States
+
+This Python script runs a Flask application that listens on 0.0.0.0:5000. It displays a web page with a list of all State objects stored in the DBStorage.
+
+The `/states_list` route is used to display the list, and the states are sorted by name. A teardown function is provided to close the SQLAlchemy session when the application context is torn down.
+
+To run the application, execute this script.
+
 """
 from models import storage
 from flask import Flask
@@ -9,21 +14,22 @@ from flask import render_template
 
 app = Flask(__name__)
 
-
 @app.route("/states_list", strict_slashes=False)
 def states_list():
-    """Displays an HTML page with a list of all State objects in DBStorage.
-    States are sorted by name.
+    """Display a List of States
+
+    This function retrieves all State objects from the database and displays them on an HTML page. The states are sorted by name.
+
+    Returns:
+        str: The rendered HTML page with the list of states.
     """
     states = storage.all("State")
     return render_template("7-states_list.html", states=states)
 
-
 @app.teardown_appcontext
 def teardown(exc):
-    """Remove the current SQLAlchemy session."""
+    """Remove the current SQLAlchemy session when the app context is torn down."""
     storage.close()
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
