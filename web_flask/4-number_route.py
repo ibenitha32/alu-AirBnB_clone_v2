59110d853ede19a7f116ba6dd5a2e00f48ef4b64
Benitha Iradukunda <116553054+ibenitha32@users.noreply.github.com>
2023-10-21 21:31:17 +0200

    fixing bugs in the flask

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,41 +1,34 @@
 #!/usr/bin/python3
-
-"""Script that starts a Flask web application"""
+"""Script that starts a Flask web application."""
 from flask import Flask
 
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """Comment"""
+def hello():
     return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    """Comment"""
+def hello_hbnb():
     return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def text_route(text):
-    """Comment"""
-    return "C {}".format(text.replace("_", " "))
+def hello_c(text):
+    return 'C ' + text.replace('_', ' ')
 
 
-@app.route('/python', strict_slashes=False)
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def text_route_python(text="is cool"):
-    """Comment"""
-    return "Python {}".format(text.replace("_", " "))
+def python(text):
+    return 'Python ' + text.replace('_', ' ')
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
-def num_route(n):
-    """Comment"""
+def number(n):
     return "{} is a number".format(n)
 
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,53 +1,56 @@
 #!/usr/bin/python3
-
-"""Script that starts a Flask web application"""
+"""Script that starts a Flask web application."""
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """Comment"""
+def hello():
+    """Returns Hello HBNB!"""
     return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    """Comment"""
+def hello_hbnb():
+    """Returns HBNB"""
     return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def text_route(text):
-    """Comment"""
-    return "C {}".format(text.replace("_", " "))
+def hello_c(text):
+    """Returns C <text>"""
+    return 'C ' + text.replace('_', ' ')
 
 
-@app.route('/python', strict_slashes=False)
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def text_route_python(text="is cool"):
-    """Comment"""
-    return "Python {}".format(text.replace("_", " "))
+def python(text):
+    """Returns Python <text>"""
+    return 'Python' + text.replace('_', ' ')
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
-def num_route(n):
-    """Comment"""
+def number(n):
+    """Returns <n> is a number"""
     return "{} is a number".format(n)
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def num_route_template(n):
-    """Comment"""
+def number_template(n):
+    """Returns HTML page if n is an integer"""
     return render_template('5-number.html', n=n)
 
 
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
-def num_route_odd_or_even(n):
-    """Comment"""
-    return render_template('6-number_odd_or_even.html', n=n)
+def number_odd_or_even(n):
+    """Returns HTML page if n is an integer and odd or even"""
+    if n % 2 == 0:
+        return render_template('6-number_odd_or_even.html',
+                               n=n, odd_even='even')
+    else:
+        return render_template('6-number_odd_or_even.html',
+                               n=n, odd_even='odd')
 
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
